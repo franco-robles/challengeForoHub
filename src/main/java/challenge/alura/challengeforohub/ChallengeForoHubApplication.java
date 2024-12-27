cc88ace08043c5b638509fd3f2b1d1c358e70e58
Franco Robles <franco_robles94@outlook.com>
2024-12-27 04:23:50 -0300

    detalles arreglados

--- src/main/java/challenge/alura/challengeforohub/ChallengeForoHubApplication.java
+++ src/main/java/challenge/alura/challengeforohub/ChallengeForoHubApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChallengeForoHubApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChallengeForoHubApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeForoHubApplication.class, args);
+    }
 
 }
