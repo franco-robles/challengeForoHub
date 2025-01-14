d836b091f7ef4cc8536ceb965c1fa5bead20ea0d
Franco Robles <franco_robles94@outlook.com>
2025-01-14 11:21:14 -0300

    configuracion de acceso y authenticationManager

--- src/main/java/challenge/alura/challengeforohub/config/SecurityConfig.java
+++ src/main/java/challenge/alura/challengeforohub/config/SecurityConfig.java
@@ -1,23 +1,56 @@
 package challenge.alura.challengeforohub.config;
 
+import challenge.alura.challengeforohub.service.SecurityFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private SecurityFilter securityFilter;
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                //.cors(cors -> cors.configurationSource(corsConfigurationSource())) // Add CORS configuration
+                .csrf(AbstractHttpConfigurer::disable) // Disable CSRF (use with caution!)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Configure stateless sessions
                 .authorizeHttpRequests((authz) -> authz
-                .anyRequest().permitAll());
-        return http.build();
+                        .requestMatchers(HttpMethod.POST, "/usuario")
+                        .permitAll()
+                        .requestMatchers(HttpMethod.POST,"/login/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
