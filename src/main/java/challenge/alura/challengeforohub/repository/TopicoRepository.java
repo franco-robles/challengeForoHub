4fff2b701740105d4f30f03a47bc5e4e1c29428f
Franco Robles <franco_robles94@outlook.com>
2025-01-14 11:25:35 -0300

    sin cambios

--- src/main/java/challenge/alura/challengeforohub/repository/TopicoRepository.java
+++ src/main/java/challenge/alura/challengeforohub/repository/TopicoRepository.java
@@ -4,4 +4,6 @@ import challenge.alura.challengeforohub.model.Topico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+
 }
